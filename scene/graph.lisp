da13e7293eb6c3dd9062bda81a1e5f2f1587008c
Tim Moore <timoore33@gmail.com>
2016-09-19 09:29:15 +0200

    Scene graph ideas

--- scene/graph.lisp
+++ scene/graph.lisp
@@ -16,4 +16,56 @@
 ;;;queues. Should the camera matrix be hooked up to every single shape, or is it time to think
 ;;;about global usets?
 
-(in-package #:lpsg)
+(in-package #:lpsg-scene)
+
+(defclass transform ()
+  ((local-matrix :input-accessor local-matrix :initarg :local-matrix
+                 :initform (sb-cga:identity-matrix))
+   (parent-matrix :input-accessor parent-matrix :initarg :parent-matrix
+                  :initform (sb-cga:identity-matrix))
+   (world-matrix :compute-function world-matrix)
+   (world-matrix-inverse :compute-function world-matrix-inverse))
+  (:metaclass compute-class))
+
+(defmethod world-matrix ((node transform))
+  (sb-cga:matrix* parent-matrix local-matrix))
+
+(defmethod world-matrix-inverse ((node transform))
+  (sb-cga:inverse-matrix (world-matrix node)))
+
+(defclass node ()
+  ((transform :accessor transform :initform (make-instance 'transform))))
+
+(defclass leaf (node)
+  ())
+
+(defclass group (node)
+  ((children :accessor children :initarg :children :initform nil)))
+
+(defgeneric add-child (parent child))
+
+(defmethod add-child ((parent group) (child node))
+  (connect (transform child) 'parent-matrix (transform group) 'world-matrix)
+  (push child (children parent)))
+
+(defgeneric remove-child (parent child))
+
+(defmethod remove-child ((parent group) (child node))
+  (disconnect (transform child) 'parent-matrix (transform group))
+  (setf (children parent) (delete child (children parent))))
+
+(defgeneric map-children (fn node))
+
+(defmethod map-children (fn (node leaf))
+  nil)
+
+(defmethod map-children (fn (node group))
+  (mapc fn (children group)))
+
+(defclass shape-node (leaf)
+  ((shape :accessor shape :initform nil)
+   (submitted :accessor submitted :initform nil :documentation "?")))
+
+(defgeneric submit-graph (root renderer) &key camera-stage)
+
+(defgeneric retract-graph (root renderer) &key camera-stage)
